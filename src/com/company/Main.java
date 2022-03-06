2de51db6715fc87e15dadf25bac5435edcef1611
Omar <omarmeciry21@gmail.com>
2022-03-07 00:03:23 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,74 @@
+package com.company;
+
+public class Main {
+//enumeration
+    enum User {
+        ADMIN_USER, HIGHER_USER,NORMAL_USER,NOT_USER
+}
+    public static void main(String[] args) {
+        int accessLevel =2;
+        // Admin: >3, Higher user>1 ,Normal: 1 ,Not user: 0
+        boolean isAdmin =true;
+
+//        if (10 != 5){
+//            System.out.println("Access Permitted!");
+//        }else{
+//            System.out.println("Access Denied!");
+//        }
+
+//        switch (accessLevel){
+//            case 3:
+//                System.out.println("Full Access");
+//                break;
+//            case 2:
+//                System.out.println("Normal User with features");
+//                break;
+//            case 1:
+//                System.out.println("Normal Access");
+//                break;
+//            case 0:
+//                System.out.println("You're not a user.");
+//                break;
+//            default:
+//                System.out.println("Invalid Value");
+//        }
+
+//        final var array = new int[]{1,5,2,3,4};
+//
+//        for (int i=0;i<=array.length-1;i++){
+//            System.out.println(array[i]);
+//        }
+
+        //While
+//        int i =0;
+//
+//        while (i<10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //Do while
+//        int i =10;
+//        do{
+//            System.out.println(i);
+//            i--;
+//        }while (i>0);
+
+        /*
+
+        *
+        **
+        ***
+        ****
+        *****
+
+        * */
+
+        for (int i= 1;i<=5;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
